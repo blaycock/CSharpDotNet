59b5d985fd5dd3a6aac6b5bf7b858230d34c4b0c
blaycock <ben.laycock@texastechfcu.org>
2019-05-15 13:18:58 -0500

    Tic Tac Toe

---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constuctor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NewGame();
+
+        }
+
+        #endregion
+        private void NewGame()
+        {
+            throw new NotImlementedException();
+        }
+    }
+}
