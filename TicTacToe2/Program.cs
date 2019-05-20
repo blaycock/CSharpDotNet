0131b5f2428a2de4b659faf8888c75f19664a011
blaycock <ben.laycock@texastechfcu.org>
2019-05-20 12:13:04 -0500

    tic tac toe v2

--- TicTacToe2/Program.cs
+++ TicTacToe2/Program.cs
@@ -8,47 +8,114 @@ namespace TicTacToe2
 {
     class Program
     {
-        static string currentPlayer = " ";
-        const int BoardSize = 3;
-        static string[,] board = new string[BoardSize, BoardSize] { { "", "", "" }, { "", "", "" }, { "", "", "" } };
-        static int TotalMoves = 0;
-        static bool gameOver = false;
+            static String[] board = new String[9];
+            static String playsAgain = "Y";
+            static int counter = 0;
 
-        static void Main(string[] args)
-        {
-            //ResetBoard();
-            StartGame();
-            DrawBoard();
-            Console.Read();
-        }
-        public static void StartGame()
-        {
-            placeMark(int RowPos, int ColPos)
+            static void initializeVariable()
+            {
+                for (int i = 0; i < 9; i++)
                 {
-                int RowIdx = RowPos - 1;
-                int ColIdx = ColPos - 1;
-                board[RowIdx, ColIdx] = currentPlayer;
-                PrintBoard();
-                //check if game is over after this
+                    board[i] = i.ToString();
+                }
+            }
+
+            static void playAgainMsg(String message)
+            {
+                Console.WriteLine(message + "Do you want to play again?");
+                if (Console.ReadLine().Equals("Y"))
+                    playsAgain.Equals("Y");
+                else
+                    playsAgain.Equals("N");
+            }
+
+            static void Main(string[] args)
+            {
+                introduction();
+                while (playsAgain.Equals("Y"))
+                {
+                    initializeVariable();
+                    while (hasWon() == false && counter < 9)
+                    {
+                        askData("X");
+                        if (hasWon() == true)
+                            break;
+                        askData("O");
+                    }
+                    if (hasWon() == true)
+                        playAgainMsg("Congratulations! You won!");
+                    else
+                        playAgainMsg("Sorry, but this was a tie game!");
+                }
+                goodBye();
+            }
+
+            static void goodBye()
+            {
+                Console.WriteLine("Thank you for playing!");
+                Console.ReadLine();
 
             }
-    public static void DrawBoard()
-            {
-                for (int i = 0; i < BoardSize; i++)
-                {
-                    Console.WriteLine("-------");
-                    for (int j = 0; j < BoardSize; j++)
-                    {
-                        Console.Write("|" + board[i, j].ToString());
-                        //Console.Write("|" + board[i, j].ToString());
-                    }
-                    Console.WriteLine("|");
-                }
-                Console.WriteLine("-------");
 
+            static void askData(String player)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Player: " + player);
+                int selection;
+
+                while (true)
+                {
+                    Console.WriteLine("Please enter your selection.");
+                    drawBoard();
+                    selection = Convert.ToInt32(Console.ReadLine());
+                    if (selection < 0 || selection > 9 || (board[selection].Equals("X") || board[selection].Equals("O")))
+                        Console.WriteLine("Invalid selection!");
+                    else
+                        break;
+                }
+                board[selection] = player;
+
+
+            }
+
+            static void drawBoard()
+            {
+                for (int i = 0; i < 7; i += 3) //Draw the board.
+                    Console.WriteLine(board[i] + "|" + board[i + 1] + "|" + board[i + 2]);
+            }
+
+            static Boolean hasWon()
+            {
+                for (int i = 0; i < 7; i += 3)
+                {
+                    if (board[i].Equals(board[i + 1]) && board[i + 1].Equals(board[i + 2]))
+                    {
+                        return true;
+                    }
+                }
+                if (board[0].Equals(board[3]) && board[3].Equals(board[6]))
+                    return true;
+                if (board[1].Equals(board[4]) && board[4].Equals(board[7]))
+                    return true;
+                if (board[2].Equals(board[5]) && board[3].Equals(board[8]))
+                    return true;
+                if (board[2].Equals(board[4]) && board[4].Equals(board[6]))
+                    return true;
+                if (board[0].Equals(board[4]) && board[4].Equals(board[8]))
+                    return true;
+                return false;
+            }
+
+
+            static void introduction()
+            {
+                Console.Title = ("Tic Tac Toe");
+                Console.WriteLine("Welcome to Tic Tac Toe.\n");
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadLine();
+                Console.Clear();
             }
 
         }
     }
-}
-
