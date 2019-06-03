35ac6db890b99bcddf2606c3407d776b2684fe65
blaycock <ben.laycock@texastechfcu.org>
2019-06-03 15:17:12 -0500

    Added to PoCos

--- PoCos/Program.cs
+++ PoCos/Program.cs
@@ -12,23 +12,42 @@ namespace PoCos
         {
             Console.WriteLine("Car Details Below:");
             Car myCar = new Car();
-            myCar.Model = "Ford ";
+            myCar.Model = "F150";
             myCar.Year = 2015;
-            myCar.Make = "F150 ";
+            myCar.Make = "Ford";
             myCar.NumberOfDoors = 2;
-
             Console.WriteLine(myCar.GetCarDetails());
-            Console.Read();
+            Console.ReadLine();
 
             Console.WriteLine("Drivers License Information Below:");
             driversLicense myDL = new driversLicense();
-            myDL.FirstName = "Ben ";
-            myDL.LastName = "Laycock ";
-            myDL.Gender = "Male ";
-            myDL.LicenseNumber = "10BB55G ";
-
+            myDL.FirstName = "Ben";
+            myDL.LastName = "Laycock";
+            myDL.Gender = "Male";
+            myDL.LicenseNumber = "10BB55G";
             Console.WriteLine(myDL.GetFullDl());
-            Console.Read();
+            Console.ReadLine();
+
+            Console.WriteLine("Book Information Below:");
+            book myBook = new book();
+            myBook.Title = "How to Kill a Mockingbird";
+            myBook.Authors = "Harper Lee";
+            myBook.Pages = 235;
+            myBook.SKU = 22566;
+            myBook.Publisher = "Penguin Books";
+            myBook.Price = 19;
+            Console.WriteLine(myBook.GetBookDetails());
+            Console.ReadLine();
+
+            Console.WriteLine("Airplane Information Below:");
+            airplane myAirplane = new airplane();
+            myAirplane.Manufacturer = "Boeing";
+            myAirplane.Model = 737;
+            myAirplane.Variant = "Convair";
+            myAirplane.Capacity = 400;
+            myAirplane.Engines = 2;
+            Console.WriteLine(myAirplane.GetPlaneDetails());
+            Console.ReadLine();
         }
     }
     class Car
@@ -49,7 +68,7 @@ namespace PoCos
         }
         public string GetCarDetails()
         {
-            return (Make + Model + Year + NumberOfDoors);
+            return (Make + ", "  + Model + ", " + Year + ", " + NumberOfDoors);
         }
     }
     class driversLicense
@@ -67,7 +86,49 @@ namespace PoCos
         }
         public string GetFullDl()
         {
-            return (FirstName + LastName  + Gender  + LicenseNumber);
+            return (FirstName + ", " + LastName + ", "  + Gender + ", "  + LicenseNumber);
+        }
+    }
+    class book
+    {
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public int Pages { get; set; }
+        public int SKU { get; set; }
+        public string Publisher { get; set; }
+        public int Price { get; set; }
+        public book()
+        {
+            Title = "Unknown";
+            Authors = "Unknown";
+            Pages = 123;
+            SKU = 4455544;
+            Publisher = "Unknown";
+            Price = 12; 
+        }
+        public string GetBookDetails()
+        {
+            return (Title + ", " + Authors + ", " + Pages + ", " + SKU + ", " + Publisher + ", " + Price);
+        }
+    }
+    class airplane
+    {
+        public string Manufacturer { get; set; }
+        public int Model { get; set; }
+        public string Variant { get; set; }
+        public int Capacity { get; set; }
+        public int Engines { get; set; }
+        public airplane()
+        {
+            Manufacturer = "Unknown";
+            Model = 111;
+            Variant = "Unknown";
+            Capacity = 4455544;
+            Engines = 3;
+        }
+        public string GetPlaneDetails()
+        {
+            return (Manufacturer + ", " + Model + ", " + Variant + ", " + Capacity + ", " + Engines);
         }
     }
 }
