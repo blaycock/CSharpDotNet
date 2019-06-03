cdaa12a5586f78680a2faf2c788103df10b80482
blaycock <ben.laycock@texastechfcu.org>
2019-06-03 18:22:41 -0500

    added new .cs to misc

---
+++ Misc Project (Udemy, Whiteboard, etc)/ReverseCloneArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc_Project__Udemy__Whiteboard__etc_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here is the array : ");
+            int[] array = {1, 2, 3, 4, 5,};
+            foreach (int a in array)
+                {
+                Console.WriteLine(a);
+                }
+            Array.Reverse(array);
+            Console.WriteLine("Reversed Array : ");
+            foreach (int value in array)
+                {
+                Console.WriteLine(value);
+                }
+            Array.Reverse(array);
+            Console.WriteLine("Cloned Array : ");
+            foreach(int value in array)
+                {
+                Console.WriteLine(value);
+                }
+            Console.ReadLine();
+        }
+    }
+}
