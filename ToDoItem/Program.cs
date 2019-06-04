56bc20f9aad5a6de7e81da57705eb4c5e5e7a0d6
blaycock <ben.laycock@texastechfcu.org>
2019-06-04 16:44:02 -0500

    Finished ToDo

--- ToDoItem/Program.cs
+++ ToDoItem/Program.cs
@@ -9,32 +9,36 @@ namespace ToDoItem
     class Program
     {
         static void Main(string[] args)
-        {
+        {  
             string question = "Would you like to add a ToDo Item? (y/n)";
             Console.WriteLine(question);
             string answer = Console.ReadLine().ToLower();
-            // put in while loop
-            Console.WriteLine ("Give me the description");
-            string description = Console.ReadLine();
-            Console.WriteLine("Give me the due date");
-            string duedate = Console.ReadLine();
-            Console.WriteLine("What is the priority? (High Med Low)");
-            string priority = Console.ReadLine();
-            ToDoItem myToDo = new ToDoItem(description, duedate, priority);
-            Console.WriteLine(" ");
-            Console.ReadLine();
-            
-            /*List<ToDoItem> toDoList = new List<ToDoItem>();
-            while (answer != 'no')
+            List<ToDoItem>myToDos = new List<ToDoItem>();
+            while (answer != "n")
             {
-
+                Console.WriteLine ("Give me the description:");
+                string description = Console.ReadLine();
+                Console.WriteLine("Give me the due date: (mm/dd/yy)");
+                string duedate = Console.ReadLine();
+                Console.WriteLine("What is the priority? (High, Normal, Low)");
+                string priority = Console.ReadLine();
+                ToDoItem myToDo = new ToDoItem(description, duedate, priority);
+                myToDos.Add(myToDo);
+                Console.WriteLine(description);
+                Console.WriteLine(duedate);
+                Console.WriteLine(priority);
+                Console.WriteLine("Do you want to submit another ToDo Item?");
+                answer = Console.ReadLine();
             }
-            foreach (ToDoItem item in toDoList)
+                Console.WriteLine();
+                Console.WriteLine("  Description  | Date(mm/dd/yy) |  Priority  ");
+                Console.WriteLine("---------------+----------------+------------");
+                Console.WriteLine("               |                |            ");
+            foreach (ToDoItem item in myToDos)
             {
-
+                Console.WriteLine(item.toDoList());
             }
-            Console.ReadKey();*/
-
+            Console.ReadKey();
         }
     }
     class ToDoItem
@@ -48,9 +52,9 @@ namespace ToDoItem
             DueDate = toDoDueDate;
             Priority = toDoPriority;
         }
-        public string GetToDoItem()
+        public string toDoList()
         {
-            return (Description + ", " + DueDate + ", " + Priority);
+            return (" " + Description +  "      " + DueDate + "        " + Priority + "      ");
         }
     }
 }
