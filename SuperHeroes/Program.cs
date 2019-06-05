d21cb1197e6ce93cf7fac12ae94314730fbc4675
blaycock <ben.laycock@texastechfcu.org>
2019-06-05 13:27:12 -0500

    add superheros

---
+++ SuperHeroes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroes
+{
+    class Program
+    {
+        class Person
+        {
+            public string name;
+            public string NickName;
+
+            public void PrintGreeting()
+            {
+                Console.WriteLine("Hi my " + name + ", you can call me" + name);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
