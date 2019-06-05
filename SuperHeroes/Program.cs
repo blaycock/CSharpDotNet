0439a4c6fc73f5947a3f4f75bbbaf9847b8cabd8
blaycock <ben.laycock@texastechfcu.org>
2019-06-05 15:04:13 -0500

    superhero work

--- SuperHeroes/Program.cs
+++ SuperHeroes/Program.cs
@@ -15,11 +15,59 @@ namespace SuperHeroes
 
             public void PrintGreeting()
             {
-                Console.WriteLine("Hi my " + name + ", you can call me" + name);
+                Console.WriteLine("Hi, my name is " + name + ", you can call me " + NickName + ".");
+                
             }
+        }
+
+        class SuperHero : Person
+        {
+             public string RealName;
+             public string SuperPower;
+
+             public void PrintGreeting2()
+             {
+                Console.WriteLine("I am " + RealName + "! My super power is " + SuperPower + "!");
+               
+             }
+
+        }
+
+        class Villain : Person
+        {
+              public string Nemesis;
+
+              public void PrintGreeting3()
+              {
+                Console.WriteLine("I am the Joker! Have you seen " + Nemesis + "?");
+                    
+              }
         }
         static void Main(string[] args)
         {
+            /*List<Person>myPerson = new List<Person>();
+            Person myPerson = new Person("Ashley", "Peter", "Richard" );
+            */
+
+            Person william = new Person();
+            william.name = "William";
+            william.NickName = "Bill";
+            Console.Write("William: ");
+            william.PrintGreeting();
+
+            SuperHero incredible = new SuperHero();
+            incredible.RealName = "Mr. Incredible";
+            incredible.SuperPower = "Super Strength";
+            Console.Write("Mr. Incredible: ");
+            incredible.PrintGreeting2();
+
+            Villain joker = new Villain();
+            joker.Nemesis = "Batman";
+            Console.Write("Joker: ");
+            joker.PrintGreeting3();
+
+
+            Console.ReadKey();
         }
     }
 }
