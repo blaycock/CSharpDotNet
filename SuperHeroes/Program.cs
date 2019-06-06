0dc2c394832d74daa00fce36fd95fa38f96b7e23
blaycock <ben.laycock@texastechfcu.org>
2019-06-06 11:58:20 -0500

    superhero work v5

--- SuperHeroes/Program.cs
+++ SuperHeroes/Program.cs
@@ -10,8 +10,8 @@ namespace SuperHeroes
     {
         class Person
         {
-            public string Name;
-            public string NickName;
+            public string Name {get; set;}
+            public string NickName{get;set;}
 
             public Person(string pName, string pNickName)
             {
@@ -21,7 +21,7 @@ namespace SuperHeroes
 
             public override string ToString()
             {
-                return "Name: " + Name;
+                return "Hi, my name is " + Name + ", you can call me " + NickName + ".";
             }
 
             public void PrintGreeting()
@@ -32,13 +32,18 @@ namespace SuperHeroes
 
         class SuperHero : Person
         {
-            public string RealName;
-            public string SuperPower;
+            public string RealName{get;set;}
+            public string SuperPower{get;set;}
 
             public SuperHero(string NickName, string Name, string sRealName, string sSuperPower) : base (Name, NickName)
             {
                 RealName = sRealName;
                 SuperPower = sSuperPower;
+            }
+
+            public override string ToString()
+            {
+                return "I am " + Name + ". When I am " + RealName + ", my super power is " + SuperPower + "!";
             }
 
             public void PrintGreeting()
@@ -49,12 +54,18 @@ namespace SuperHeroes
 
         class Villain : Person
         {
-            public string Nemesis;
+            public string Nemesis {get;set;}
 
             public Villain(string NickName, string Name, string vNemesis) : base (Name, NickName)
                 {
                     Nemesis = vNemesis;
                 }
+
+            public override string ToString()
+            {
+                return "I am " + Name + " have you seen " + Nemesis + "?";
+            }
+
             public void PrintGreeting()
             {
                 Console.WriteLine("I am " + Name + " have you seen " + Nemesis + "?");
@@ -62,12 +73,48 @@ namespace SuperHeroes
         }
         static void Main(string[] args)
         {
+            List<Person> HumanList = new List<Person>();
+            Person nPerson = new Person("Ashley", "Ash");
+            Person nPerson1 = new Person("Fred", "Mr. Fred");
+            Person nPerson2 = new Person("Mindy", "Izzy");
+            Person nPerson3 = new Person("Charlie", "Pat Pat");
+            Person[] persons = {new Person("Ashley","Ash"), new Person("Fred", "Mr. Fred"), new Person("Mindy", "Izzy"), new Person("Charlie", "Pat Pat")};
+            HumanList.AddRange(persons);
+            foreach (Person a in persons)
+                {
+                Console.WriteLine(a);
+                
+                }
+
+            List<SuperHero> SuperList = new List<SuperHero>();
+            SuperHero newSuperHero = new SuperHero("", "Perter Parker", "Spider Man", "Spider Power");
+            Person nPerson1 = new Person("Fred", "Mr. Fred");
+            Person nPerson2 = new Person("Mindy", "Izzy");
+            Person nPerson3 = new Person("Charlie", "Pat Pat");
+            Person[] persons = {new Person("Ashley","Ash"), new Person("Fred", "Mr. Fred"), new Person("Mindy", "Izzy"), new Person("Charlie", "Pat Pat")};
+            HumanList.AddRange(persons);
+            SuperList.Add(newSuperHero);
+            foreach (Person a in SuperList)
+                {
+                Console.WriteLine(a);
+                }
+
+            List<Villain> VillainList = new List<Villain>();
+            Villain newVillain = new Villain("", "Dr. Octopus", "Spider Man");
+            VillainList.Add(newVillain);
+            foreach (Person a in VillainList)
+                {
+                Console.WriteLine(a);
+                }
+
+            /*
             Person nPerson = new Person("Bill", "William");
             nPerson.PrintGreeting();
             SuperHero nSuperHero = new SuperHero(" ","Wade Turner", "Mr. Incredible", "Super Strength");
             nSuperHero.PrintGreeting();
             Villain nVillain = new Villain(" ", "The Joker", "Batman");
             nVillain.PrintGreeting();
+            */
 
             Console.ReadKey();
         }
