f3da1b58bc46c9b13b2383fdb36663ff9928a9b5
blaycock <ben.laycock@texastechfcu.org>
2019-06-12 16:25:36 -0500

    car lot work

--- CarLot2/Program.cs
+++ CarLot2/Program.cs
@@ -8,6 +8,33 @@ namespace CarLot2
 {
     class Program
     {
+	public class CarLot
+	{
+		public string name {get; set;}
+		public string vehicleList() 
+		{
+			return "Truck", "SUV", "Crossover", "Hatchback", "Sedan", "Coupe", "Wagon", "Convertable";
+		}
+	}
+	abstract class Vehicle
+	{
+		abstract int licenseNumber {get; set;}
+		abstract string make {get; set;}
+		abstract string model {get; set;}
+		abstract int price {get; set;}
+	}
+	public class Truck : Vehicle
+	{
+		public int bedSize {get; set;}
+	
+	}
+	public class Car : Vehicle
+	{
+		public string type {get; set;}
+		public int numDoors {get; set;}
+	}
+	
+
         static void Main(string[] args)
         {
         }
