921b4b315edaa6c98dbced40064bc1cfb0e389b5
blaycock <ben.laycock@texastechfcu.org>
2019-06-14 15:39:07 -0500

    Carlot work

--- CarLot2/Program.cs
+++ CarLot2/Program.cs
@@ -11,29 +11,106 @@ namespace CarLot2
 	class CarLot
 	{
 		public string name {get; set;}
+
+        public CarLot(string lotName)
+        {
+            name = lotName;
+        }
 		public List<Vehicle> typeVehicle = new List<Vehicle>();
+
+        public virtual void AddCars(Vehicle v1)
+        {
+           typeVehicle.Add(v1);
+        }
+
+        public virtual void PrintInventory()
+        {
+            Console.WriteLine("{0} parking lot included the following vehicles: ", name);
+            foreach (Vehicle vehicles in typeVehicle)
+            {
+                Console.WriteLine(vehicles.VehicleDescription());
+            }
+        }
 	}
 	abstract class Vehicle
 	{
-		abstract int licenseNumber {get; set;}
-		abstract string make {get; set;}
-		abstract string model {get; set;}
-		abstract int price {get; set;}
-	}
-	public class Truck : Vehicle
+		public string LicenseNumber {get; set;}
+		public string make {get; set;}
+		public string model {get; set;}
+		public int price {get; set;}
+
+        public Vehicle(string licenseNumber, string Make, string Model, int Price)
+        {
+            LicenseNumber = licenseNumber;
+            make = Make;
+            model = Model;
+            price = Price;
+        }
+        
+        public virtual string VehicleDescription()
+        {
+            string VehicleDescription = make + model + price + LicenseNumber;
+            return VehicleDescription;
+        }
+    }
+	class Truck : Vehicle
 	{
 		public int bedSize {get; set;}
-	
+
+        public Truck(string licenseNumber, string Make, string Model, int Price, int BedSize) : base (licenseNumber, Make, Model, Price)
+        {
+            bedSize = BedSize;
+            LicenseNumber = licenseNumber;
+            make = Make;
+            model = Model;
+            price = Price;
+        }
+
+        public override string VehicleDescription()
+        {
+            string VehicleDescription ="Truck: " + make + model + price + bedSize;
+            return VehicleDescription;
+        }
 	}
-	public class Car : Vehicle
+	class Car : Vehicle
 	{
 		public string type {get; set;}
 		public int numDoors {get; set;}
-	}
-	
 
+        public Car (string licenseNumber, string Make, string Model, string Type, int Price, int NumDoors) : base (licenseNumber, Make, Model, Price)
+        {
+            LicenseNumber = licenseNumber;
+            make = Make;
+            model = Model;
+            price = Price;
+            type = Type;
+            numDoors = NumDoors;
+        }
+
+            public override string VehicleDescription()
+            {
+                string VehicleDescription = "Car: " + make + " " + model + " " + price + " " + type + " " + numDoors + " " + LicenseNumber;
+                return VehicleDescription;
+            }
+        }
+        
         static void Main(string[] args)
         {
+            CarLot UsedLot = new CarLot("Used Auto");
+            Vehicle v1 = new Truck("HG63TV", "Hyundai", "Elantra", 24000, 4);
+            Vehicle v2 = new Car("HG63TV", "Hyundai", "Elantra", "Sedan", 24000, 4);
+            UsedLot.AddCars(v1);
+            UsedLot.AddCars(v2);
+            UsedLot.PrintInventory();
+            
+
+            CarLot NewLot = new CarLot("New Auto");
+            Vehicle v3 = new Truck("HG63TV", "Hyundai", "Elantra", 24000, 4);
+            Vehicle v4 = new Car("HG63TV", "Hyundai", "Elantra", "Sedan", 24000, 4);
+            NewLot.AddCars(v3);
+            NewLot.AddCars(v4);
+            NewLot.PrintInventory();
+            Console.Read();
         }
     }
 }
